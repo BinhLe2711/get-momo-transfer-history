14a5f64d0fdc43bd3006085d0e055f83b9aa975b
binh27112004@gmail.com <binh27112004@gmail.com>
2020-09-12 22:23:14 +0700

    fix regex

--- gmail.php
+++ gmail.php
@@ -2,7 +2,7 @@
 <?php
 
     $username = 'binh27112004@gmail.com'; //Your email
-    $password = '27112004'; //Your password
+    $password = 'ekko2711'; //Your password
 
 ?>
 
@@ -105,7 +105,7 @@ td, th {
     <div id="listData" class="list-form-container">
             <?php
         $connection = imap_open('{imap.gmail.com:993/imap/ssl}INBOX', $username, $password) or die('Cannot connect to Gmail: ' . imap_last_error());
-        $emailData = imap_search($connection, 'FROM "no-reply@momo.vn" SINCE "'.$date.'"');
+        $emailData = imap_search($connection, 'FROM "no-reply@momo.vn"');
         
         if (! empty($emailData)) {
             ?>
@@ -114,6 +114,7 @@ td, th {
             foreach ($emailData as $emailIdent) {
                 $overview = imap_fetch_overview($connection, $emailIdent, 0);
                 if(preg_match("/Giao dịch thành công/",imap_utf8($overview[0]->subject))) continue;
+                if(!preg_match("/Bạn vừa nhận được tiền từ/",imap_utf8($overview[0]->subject))) continue;
                 $message = ((imap_fetchbody($connection, $emailIdent,1)));
                 $message = preg_replace( "/\s+/", " ", $message);
                 preg_match('/(?<=li= ne-height: 1.2em; font-weight: 500;"> )(.*?)(?= <\/td>)/', $message, $matches);
@@ -129,7 +130,7 @@ td, th {
                 $phone =  str_replace("=",'%',$matches[1]);
                 preg_match('/(?<=gian<\/div> <\/td> <td class=3D"" style=3D"padding-top: 5px; padding-bottom: 5px; font-si= ze: 14px; font-family: Helvetica Neue, Arial, sans-serif; color: #3C4043; t= ext-align: left; line-height: 1.55em;" width=3D"50%"> <div style=3D"color:#3C4043;margin:0px;font-size:12px;li= ne-height:22px; font-weight: normal; font-size: 15px;"> )(.*?)(?=<\/div>)/', $message, $matches);
                 $time =  str_replace("=",'%',$matches[1]);
-                preg_match('/(?<=ch=C3=BAc<\/div> <\/td> <td class=3D"" style=3D"padding-top: 5px; padding-bottom: 5px; font-si= ze: 14px; font-family: Helvetica Neue, Arial, sans-serif; color: #3C4043; t= ext-align: left; line-height: 1.55em;" width=3D"50%"> )(.*?)(?=<\/div>)/', $message, $matches);
+                preg_match('/(?<=L=E1=BB=9Di nh=E1=BA=AFn<\/div> <\/td> <td class=3D"" style=3D"padding-top: 5px; padding-bottom: 5px; font-si= ze: 14px; font-family: Helvetica Neue, Arial, sans-serif; color: #3C4043; t= ext-align: left; line-height: 1.55em;" width=3D"50%"> <div style=3D"color:#3C4043;margin:0px;font-size:12px;li= ne-height:22px; font-weight: normal; font-size: 15px;"> )(.*?)(?=<\/div>)/', $message, $matches);
                 $content =  str_replace('<div style="color:#3C4043;margin:0px;font-size:12px;li% ne-height:22px; font-weight: normal; font-size: 15px;"> ','',urldecode(str_replace("=",'%',$matches[1])));
                 $date = date("d F, Y", strtotime($overview[0]->date));
                 ?>
